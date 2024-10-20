fbacd7563851d2c6dbaf752f31790e23e9ebfb80
Saras-12 <Saraswathi.gantapaka_ug2023@ashoka.edu.in>
2024-10-20 22:39:44 +0530

    HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -7,19 +7,16 @@ private:
     std::vector<bool> isDeleted;  
     int size;  
     int count;  
-    static const double loadFactorThreshold;  // Change here
+    static const double loadFactorThreshold;  
 
-    // Hash function
     int hashFunction(int key) {
         return key % size;
     }
 
-    // Quadratic probing to resolve collisions
     int quadraticProbing(int key, int i) {
         return (hashFunction(key) + i * i) % size;
     }
 
-    // Check if a number is prime (for resizing)
     bool isPrime(int num) {
         if (num <= 1) return false;
         for (int i = 2; i * i <= num; ++i) {
@@ -28,7 +25,6 @@ private:
         return true;
     }
 
-    // Find the next prime number larger than the current size
     int nextPrime(int num) {
         while (!isPrime(num)) {
             num++;
@@ -36,11 +32,10 @@ private:
         return num;
     }
 
-    // Resize the hash table
     void resize() {
         int newSize = nextPrime(2 * size);
-        std::vector<int> oldTable = table;
-        std::vector<bool> oldIsDeleted = isDeleted;
+        std::vector<int> oldTable = table;  
+        std::vector<bool> oldIsDeleted = isDeleted;  
         table = std::vector<int>(newSize, -1);
         isDeleted = std::vector<bool>(newSize, false);
         size = newSize;
@@ -54,7 +49,6 @@ private:
     }
 
 public:
-    // Constructor
     HashTable(int initialSize) {
         size = nextPrime(initialSize);
         table = std::vector<int>(size, -1);
@@ -62,10 +56,9 @@ public:
         count = 0;
     }
 
-    // Insert a key into the hash table
     void insert(int key) {
         if ((double)count / size > loadFactorThreshold) {
-            resize();
+            resize();  
         }
 
         int i = 0;
@@ -73,8 +66,8 @@ public:
         while (i < size) {
             index = quadraticProbing(key, i);
             if (table[index] == -1 || isDeleted[index]) {
-                table[index] = key;
-                isDeleted[index] = false;
+                table[index] = key;  
+                isDeleted[index] = false;  
                 count++;
                 return;
             } else if (table[index] == key) {
@@ -87,7 +80,6 @@ public:
         std::cout << "Max probing limit reached!" << std::endl;
     }
 
-    // Remove a key from the hash table
     void remove(int key) {
         int i = 0;
         int index;
@@ -98,7 +90,7 @@ public:
                 isDeleted[index] = true;  
                 count--;
                 return;
-            } else if (table[index] == -1 && !isDeleted[index]) {
+            } else if (table[index] == -1) {
                 std::cout << "Element not found" << std::endl;
                 return;
             }
@@ -108,7 +100,6 @@ public:
         std::cout << "Element not found" << std::endl;
     }
 
-    // Search for a key and return its index
     int search(int key) {
         int i = 0;
         int index;
@@ -124,10 +115,9 @@ public:
         return -1;  
     }
 
-    // Print the hash table
     void printTable() {
         for (int i = 0; i < size; ++i) {
-            if (table[i] == -1 || isDeleted[i]) {
+            if (table[i] == -1) {
                 std::cout << "- ";
             } else {
                 std::cout << table[i] << " ";
